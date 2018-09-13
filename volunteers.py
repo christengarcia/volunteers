590046f6d31b8dac0d3c910a18f66d19a2b04c95
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-09-13 12:44:39 +1000

    Initial commit

---
+++ volunteers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Application for the input of volunteers for the company outreach projects.
+Program records transactions for each payment and saves to .csv file.
+A transaction id will also be recorded with the name and payment.
+"""
+           
+
+
+                           
